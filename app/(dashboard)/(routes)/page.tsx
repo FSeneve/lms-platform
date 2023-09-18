917ea8dd07498c9d1ff246c61e83ae00f9eb6aea
Francis <francisseneve@gmail.com>
2023-09-18 16:35:20 +0100

    Initial setup

---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+    <div className="text-center">
+      <p className="bg-red-500">Hello world</p>
+      <Button>
+        Click me
+      </Button>
+    </div>
+  )
+}
