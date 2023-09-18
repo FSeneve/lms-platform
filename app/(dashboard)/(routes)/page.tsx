0f43b2109d5cc3b2e800d5a77f41597dc96f3fc1
Francis <francisseneve@gmail.com>
2023-09-18 21:30:12 +0100

    Added clerk auth

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,13 +1,12 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from '@clerk/nextjs';
 
 
 export default function Home() {
   return (
-    <div className="text-center">
-      <p className="bg-red-500">Hello world</p>
-      <Button>
-        Click me
-      </Button>
+    <div>
+      <UserButton
+       afterSignOutUrl='/'
+      />
     </div>
   )
 }
